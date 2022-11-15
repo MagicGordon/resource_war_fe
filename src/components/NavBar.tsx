f6af1903009a42cffd65de7f8dcf61d636263fa7
MagicGordon <19465870+MagicGordon@users.noreply.github.com>
2022-11-15 11:27:59 +0800

    remove bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,18 +9,7 @@ export function NavBar() {
   return (
     <nav className="navbar py-4 px-4 bg-base-100">
       <div className="flex-1">
-        <a href="http://movedid.build" target="_blank">
-          {/* <Image src="/logo.png" width={64} height={64} alt="logo" /> */}
-        </a>
-        <ul className="menu menu-horizontal p-0 ml-5">
-          <NavItem href="/" title="AddrAggregatorManager" />
-          <NavItem href="/endpoint" title="EndpointManager" />
-          <NavItem href="/did_querier" title="DIDQuerier" />
-          <li className="font-sans font-semibold text-lg">
-            <a href="https://github.com/NonceGeek/MoveDID/" target="_blank">Source Code</a>
-            <a href={MODULE_URL} target="_blank">Contract on Explorer</a>
-          </li>
-        </ul>
+        
       </div>
       <AptosConnect />
     </nav>
